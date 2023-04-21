488e11bc5a39461ffa060a3e3601e80d370ee1d6
FreeMoneyHyb <83422129+FreeMoneyHyb@users.noreply.github.com>
2023-04-21 12:34:50 -0500

    made names more randomized
    names are now more randomized before it was a random 3 letter string now its alot more randomized some examples of the expected output
    
    forest_987
    appleXD
    rainbow457
    mountain_YT
    unicorn_123
    pizza_YT
    sunflowerXD
    elephant_345
    ocean_XX
    tigerYT
    galaxy_XX
    pizza_890
    mountain_XX
    sunflower_321
    rainbow_XD
    galaxy_YY
    elephant_789
    oceanXD
    tiger_YY
    rainbow_234
    pizza_YY
    sunflower_456
    forestXD
    galaxy_234
    mountain_YT
    apple_XX
    unicornXD
    ocean_YY
    tiger_678
    rainbowXD
    pizza_YY
    mountain_234
    elephant_YT
    sunflower_XX
    apple_YY
    galaxy_YY

--- namer.py
+++ namer.py
@@ -6,9 +6,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 # open the file with the list of alts
 with open('alts.txt', 'r') as f:
     lines = f.readlines()
+
+abbreviations = ['YT', 'XD', '_']  # only keep YT, XD, and _ in the list
 
 # loop through each line in the file
 for line in lines:
@@ -19,6 +22,8 @@ for line in lines:
         random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=ok))
         driver.get('https://www.minecraft.net/en-us/login')
         word = random_word.RandomWords().get_random_word()
+        if random.random() < 0.05:
+            word = random.choice(['_', 'x']) + word
         sign_in_button = driver.find_element(By.XPATH, '//a[@data-aem-contentname="Sign in with "]')
         sign_in_button.click()
         wait = WebDriverWait(driver, 10)
@@ -57,9 +62,19 @@ for line in lines:
         profile_link = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@data-aem-contentname="Profile Name"]')))
         profile_link.click()
         profile_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@data-testid="profile-name-input"]')))
+        rand_num = random.random()
+        if rand_num < 0.45:
+            profile_name = f'{word}{random_string}'
+        elif rand_num < 0.9:
+            profile_name = f'{word}{random.randint(100, 999)}'
+        else:
+            profile_name = f'{word}{random.choice(abbreviations) if random.random() < 0.1 else ""}'  # Add abbreviations with a 10% probability
+
         profile_input.clear()
-        profile_input.send_keys(f'{word}{random_string}')
-        set_profile_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="ChangeNameButton"]')))
+        profile_input.send_keys(profile_name)
+
+        set_profile_button = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="ChangeNameButton"]')))
         set_profile_button.click()
         time.sleep(3)
         driver.quit()
